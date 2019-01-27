f6d540cf98bd8090c00e490e08943acd2e2babd3
Shivil Sachdeva <shivilsachdeva@Shivils-iMac.local>
2019-01-26 21:27:46 -0500

    responsive full-screen navbar up to date only issue: not covering 100% height

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 console.log("Connected");
+
+
+
+
+	
+var t1 = new TimelineMax({paused: true});
+
+t1.to(".one", 0.5, {
+    y: 3,
+    rotation: 45,
+    ease: Expo.easeInOut,
+    // delay: 0
+});
+
+t1.to(".two", 0.3, {
+    y: 3,
+    rotation: -45,
+    ease: Expo.easeInOut,
+    delay: -0.449
+});
+
+t1.to(".menu", 0.3, {
+    top: "0%",
+    position: "absolute",
+    ease: Expo.easeInOut,
+    delay: -0.2
+});
+
+// t1.to(".data", 0.1, {
+//     x: 500,
+//     y: 200,
+//     ease: Expo.easeInOut,
+//     delay: 0
+// });
+
+t1.staggerFrom(".menu ul li", 0.6, {x: -300, opacity: 0, ease:Expo.easeInOut}, 0.2);
+
+t1.reverse(); 
+$(document).on("click", ".toggle-btn", function() {
+    t1.reversed(!t1.reversed());
+}); 
